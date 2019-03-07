75fc1a8ce079336763db9cfdc730fc860db30275
altrdev <alessandro.travi@gmail.com>
2019-03-07 23:18:22 +0100

    [feat]: add firt component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import './App.css';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(1200 + theme.spacing.unit * 2 * 2)]: {
+      width: 1200,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3,
+    },
+  }
+});
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <article>
-        <h1>I will be back soon!</h1>
-        <div>   
-          <p>Sorry for the inconvenience but I&rsquo;m performing some maintenance at the moment. I will be back online shortly!</p>
-          <p>&mdash; altrdev <span role="img" aria-label="alien">👾</span></p>
-        </div>
-      </article>
+      <React.Fragment>
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography component="h1" variant="h4" align="center">
+              I will be back soon!
+            </Typography>
+            <React.Fragment>
+              <Typography variant="subtitle1" align="center">
+                Sorry for the inconvenience but I&rsquo;m performing some maintenance at the moment. I will be back online shortly!
+              </Typography>
+              <Typography variant="subtitle1">
+                &mdash; altrdev <span role="img" aria-label="alien">👾</span>
+              </Typography>
+            </React.Fragment>
+          </Paper>
+        </main>
+      </React.Fragment>
+      
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles) (App);
