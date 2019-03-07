7b1f55a277ccf83f2854128657792c195cecfad9
altrdev <alessandro.travi@gmail.com>
2019-03-07 15:53:59 +0100

    [feat]: Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <article>
+        <h1>I will be back soon!</h1>
+        <div>   
+          <p>Sorry for the inconvenience but I&rsquo;m performing some maintenance at the moment. I will be back online shortly!</p>
+          <p>&mdash; altrdev <span role="img" aria-label="alien">👾</span></p>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default App;
