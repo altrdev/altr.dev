da1dd98bae679808826cb5d04691f4f0262a2681
altrdev <alessandro.travi@gmail.com>
2019-03-25 10:34:58 +0100

    [fix]: linkedin profile url

--- src/properties.js
+++ src/properties.js
@@ -24,7 +24,7 @@ export const properties = {
         workTitle: "Crazy Software Engineer",
         description: "I’m a software engineer always willing to put myself out there and push myself beyond my limits.\nRight after high school diploma, while working as postman, I started challenging myself with both personal side-projects and freelance consulting.\nI'm not sure what being a 'self starter' means, but people tends to call me that.",
         icons: [
-            createData(mdiLinkedinBox, "https://www.linkedin.com/in/alessandrotravi/?locale=en_US"),
+            createData(mdiLinkedinBox, "https://www.linkedin.com/in/alessandrotravi/en"),
             createData(mdiGithubCircle, "https://github.com/altrdev/"),
             createData(mdiInstagram, "https://www.instagram.com/alessandro.travi/"),
             createData(mdiTwitter, "https://twitter.com/altrdev")
