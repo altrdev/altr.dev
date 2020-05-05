100ec561ca1ecad69590b0c9b80242b33a2bf95c
altrdev <alessandro.travi@gmail.com>
2020-05-05 22:58:45 +0200

    [fix]: changed mail

--- src/properties.js
+++ src/properties.js
@@ -32,7 +32,7 @@ export const properties = {
         tableRows: [
             createData("Age", 30),
             createData("Country", "Como Lake, Italy"),
-            createData("Email", <Obfuscate email="alessandro.travi+dev@gmail.com"/>),
+            createData("Email", <Obfuscate email="info@altr.dev"/>),
             createData("Website", "https://altr.dev")
         ]
     },
