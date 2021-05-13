f54747cb399fca9acfc1caf109b8cef733900402
Alessandro Travi <altrdev@MBP-AT.fritz.box>
2021-05-13 17:51:00 +0200

    [feat]: animate css

--- components/Layout.js
+++ components/Layout.js
@@ -23,7 +23,8 @@ export default class Layout extends React.Component {
                     <meta name="yandex-verification" content="c5acf8bbe12bc1d5" />
                     <meta name="msvalidate.01" content="D5D6C71B573ADD7743E2BFEF1E56E6D9" />
 
-                    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="800b48d7-e6d0-4aac-b9ac-94d622bdac24" type="text/javascript" async></script>
+                    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="800b48d7-e6d0-4aac-b9ac-94d622bdac24" type="text/javascript" async/>
+                    <script id="CookieDeclaration" src="https://consent.cookiebot.com/800b48d7-e6d0-4aac-b9ac-94d622bdac24/cd.js" type="text/javascript" async />
                     <script type="text/plain" data-cookieconsent="statistics" dangerouslySetInnerHTML={{ __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
                 ga('create', 'UA-135914500-1', 'auto');
                 ga('send', 'pageview');` }} />
