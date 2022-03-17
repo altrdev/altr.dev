871d5bc66b0291ff6f92640dd48a3c356c345c54
altrdev <alessandro.travi@gmail.com>
2022-03-17 19:13:37 +0100

    [fix]: public env google id

--- components/Layout.js
+++ components/Layout.js
@@ -10,14 +10,14 @@ const Layout = ({children, title = info.title}) => {
 
     return (
         <>
-            <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive' />
+            <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} strategy='afterInteractive' />
             <Script id="google-analytics" strategy='afterInteractive'>
                 {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
-                gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+                gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
                 `}
             </Script>
             <Head>
