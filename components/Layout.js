3e74318ac2cfe81170050ffa7109d753a93cee16
altrdev <alessandro.travi@gmail.com>
2022-03-17 18:49:19 +0100

    [feat]: new google tag and remove cookiebot

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Script from 'next/script'
 import theme from "../utils/theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import {ThemeProvider} from "@material-ui/core/styles";
@@ -26,14 +27,17 @@ const Layout = ({children, title = info.title}) => {
                 <meta name="yandex-verification" content="c5acf8bbe12bc1d5" />
                 <meta name="msvalidate.01" content="D5D6C71B573ADD7743E2BFEF1E56E6D9" />
 
-                <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="800b48d7-e6d0-4aac-b9ac-94d622bdac24" type="text/javascript" async/>
-                <script type="text/plain" data-cookieconsent="statistics" dangerouslySetInnerHTML={{ __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-135914500-1', 'auto');
-            ga('send', 'pageview');` }} />
+                <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive' />
+                <Script id="google-analytics" strategy='afterInteractive'>
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
 
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                <script id="CookieDeclaration" src="https://consent.cookiebot.com/800b48d7-e6d0-4aac-b9ac-94d622bdac24/cd.js" type="text/javascript" async />
+                    gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+                    `}
+                </Script>
+                
                 <title>{ title }</title>
             </Head>
             <ThemeProvider theme={theme}>
