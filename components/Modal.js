86184374af0573fe476000d3350c43b22d63e79e
altrdev <alessandro.travi@gmail.com>
2022-11-16 11:00:29 +0100

    feat: circular progress

--- components/Modal.js
+++ components/Modal.js
@@ -1,47 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import { Modal, Box } from '@mui/material';
-import mql from '@microlink/mql';
+import React, { useState, useEffect } from "react";
+import { Modal, Box, CircularProgress } from "@mui/material";
+import mql from "@microlink/mql";
 
-export default function ModalResume({show, toggleModal}) {
+import createStyles from '@mui/styles/createStyles';
+import makeStyles from '@mui/styles/makeStyles';
 
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        const fetchData = async () => {
-            const { status, data } = await mql('https://altr.dev/pdf?obfuscate=false', { pdf: 'true', scale: '1', margin: '0.4cm', format: 'letter' });      
-            setData(data.pdf.url);
+const useStyles = makeStyles(theme => {
+    return createStyles({
+        boxModal: {
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "90%",
+            height: "90%",
+            backgroundColor: theme.palette.primary.resumeMain,
+            border: "2px solid",
+            borderColor: theme.palette.primary.main,
+            boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+            padding: 4,
+        },
+        divCircular: {
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
         }
-        if(show) {
-            fetchData();
-        }
-    }, [show])
-    
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '90%',
-        height: '90%',
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
+    })
+});
 
-    return (
-        <Modal
-            open={show}
-            onClose={toggleModal}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box sx={style}>
-                <iframe
-                width="100%"
-                height="100%"
-                    src={data}
-                ></iframe>
-            </Box>
-        </Modal>
-    );
+export default function ModalResume({ show, toggleModal }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const classes  = useStyles();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { status, data } = await mql(
+        "https://altr.dev/pdf?obfuscate=false",
+        { pdf: "true", scale: "1", margin: "0.4cm", format: "letter" }
+      );
+      setData(data.pdf.url);
+      setLoading(false);
+    };
+    if (show) {
+      fetchData();
+    }
+  }, [show]);
+
+  return (
+    <Modal
+      open={show}
+      onClose={toggleModal}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box className={classes.boxModal}>
+        {loading ? (
+            <div className={classes.divCircular}>
+                <CircularProgress color="primary"/>
+            </div>
+        ) : (
+          <iframe width="100%" height="100%" src={data}></iframe>
+        )}
+      </Box>
+    </Modal>
+  );
 }
